067ed65ecefb4fdcb588df9db971f333ddde33f7
ninhph96 <32106228+ninhph96@users.noreply.github.com>
2026-03-14 01:47:44 +0900

    Update page.tsx

--- app/campaign/[id]/page.tsx
+++ app/campaign/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client'
-
+export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
 import { useState, useEffect, use } from 'react'
 import { useRouter } from 'next/navigation'
 import Image from 'next/image'
